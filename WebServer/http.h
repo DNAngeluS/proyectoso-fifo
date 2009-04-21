6b0f845fc78afeddef3c5c5f038cb2083306b661
luciomf@gmail.com <luciomf@gmail.com@9df64b58-23b5-11de-99f3-63f2d503d578>
2009-04-21 00:54:58 +0000

    mismo commit anterior

--- WebServer/http.h
+++ WebServer/http.h
@@ -19,9 +19,9 @@ int RecibirNBloque		(SOCKET sockfd, LPVOID bloque, DWORD nBytes);
 int EnviarArchivo		(SOCKET sockRemoto, HANDLE fileHandle);
 HANDLE BuscarArchivo	(char *filename);
 
+int httpOk_send			(SOCKET sockfd, msgGet *getInfo);
 int httpGet_recv		(SOCKET sockfd, msgGet *getInfo);
 int httpTimeout_send	(SOCKET sockfd, int protocolo);
-int httpOk_send			(SOCKET sockfd, int protocolo);
-int httpNotFound_send	(SOCKET sockfd, int protocolo);
+int httpNotFound_send	(SOCKET sockfd, msgGet *getInfo);
 
-char *pathUnixToWin		(const char *dir, char *path);
+char *pathUnixToWin		(const char *dir, char *path);>
