cc0467ff9970c52383ef5f3abaa79989bba8aeeb
luciomf@gmail.com <luciomf@gmail.com@9df64b58-23b5-11de-99f3-63f2d503d578>
2009-05-03 19:59:01 +0000

    Actualizado con las estructuras realizadas el viernes

--- Motor de Busqueda/irc.h
+++ Motor de Busqueda/irc.h
@@ -8,6 +8,37 @@
 #ifndef _IRC_H
 #define	_IRC_H
 
+#define MAX_UUID 35
+
+typedef struct {
+    char descriptorID [16];
+    int payloadDesc;
+    long payloadLen;
+    void *payload;
+}headerIRC;
+typedef struct {
+    char palabra [MAX_PATH];
+    char url [MAX_PATH];
+    char titulo [MAX_PATH];
+    char desc [MAX_PATH];
+    char UUID [MAX_UUID];
+}soPalabrasHTML;
+typedef struct {
+    char palabra [MAX_PATH];
+    char url [MAX_PATH];
+    int tipo;
+    char fomarmato [4];
+    long tamano;
+}soPalabrasArchivos;
+typedef struct {
+    char UUID [MAX_UUID];
+    char html [MAX_PATH];
+}hostsCodigo;
+typedef struct {
+    char host [MAX_PATH];
+    long uts;
+}hostsExpiracion;
+
 int ircRequest_send ();
 int ircRequest_recv ();
 int ircResponse_send ();
