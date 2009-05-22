a2391bb957df352b704a26e935a011677a833f60
marianoscheinkman <marianoscheinkman@9df64b58-23b5-11de-99f3-63f2d503d578>
2009-05-22 14:18:03 +0000

    [Web Store] Se sube webstore.c vacio y webstore.h que contiene las estructuras del paquete que recibira del crawler.

---
+++ Motor de Busqueda/WebStore/webstore.c
@@ -0,0 +1,16 @@
+/* 
+ * File:   webstore.c
+ * Author: marianoyfer
+ *
+ * Created on 15 de mayo de 2009, 11:46
+ */
+#include "webstore.h"
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
---
+++ Motor de Busqueda/WebStore/webstore.h
@@ -0,0 +1,47 @@
+/* 
+ * File:   webstore.h
+ * Author: marianoyfer
+ *
+ * Created on 15 de mayo de 2009, 11:47
+ */
+
+#ifndef _WEBSTORE_H
+#define	_WEBSTORE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>
+#include <sys/timeb.h>
+#include <fcntl.h>
+#include <sys/file.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <ctype.h>
+
+#define INVALID_SOCKET -1
+#define MAX_PATH 256
+#define SOCKET_ERROR -1
+#define BUF_SIZE 1024
+#define MAX_HTMLCODE 4096
+
+typedef struct {
+    char URL            [MAX_PATH];
+    char titulo         [MAX_PATH];
+    char descripcion    [MAX_PATH];
+    char *palabras      [MAX_PATH];
+    char htmlCode       [MAX_HTMLCODE];
+} crawler_URL_HTML;
+
+typedef struct {
+    char URL            [MAX_PATH];
+    char length         [20];
+    char formato        [MAX_PATH];
+    char *palabras      [MAX_PATH];
+} crawler_URL_ARCHIVOS;
+
+#endif	/* _WEBSTORE_H */
+
