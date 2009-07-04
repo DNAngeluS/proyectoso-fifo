b90cdd4845dec5e75c83952b7942e7196b8367e8
marianoscheinkman <marianoscheinkman@9df64b58-23b5-11de-99f3-63f2d503d578>
2009-07-04 21:39:19 +0000

    [Query Processor] Se termina el tema de logeo.

---
+++ Motor de Busqueda/QueryProcessor/log.c
@@ -0,0 +1,34 @@
+/*
+ * File:   log.c
+ * Author: mariano
+ *
+ * Created on 4 de julio de 2009, 14:05
+ */
+#include "log.h"
+extern mutex_t logMutex;
+
+int WriteLog(int log, char *proceso, pid_t pid, thread_t thread, char *descripcion, char *tipo)
+{
+    char buffer[BUF_SIZE];
+    int error = 0, n;
+    char fecha[20];
+    time_t tiempo = time(NULL);
+
+    memset(buffer, '\0', sizeof(buffer));
+    memset(fecha, '\0', sizeof(fecha));
+
+    strftime(fecha, sizeof(fecha), "[%H:%M:%S]", localtime(&tiempo));
+
+    sprintf(buffer, "%s - %s [%d][%d]:%s: %s\n", fecha, proceso, pid, thread, tipo, descripcion);
+
+    lseek(log, 0L, 2);
+
+    mutex_lock(&logMutex);
+    if ((n = write(log, buffer, strlen(buffer))) < 0)
+        error = 1;
+    mutex_unlock(&logMutex);
+
+    printf("%s.%s", descripcion, strcmp(tipo, "INFO") == 0? " ":"\n");
+
+    return error? -1: 0;
+}
--- Motor de Busqueda/QueryProcessor/qp.c
+++ Motor de Busqueda/QueryProcessor/qp.c
@@ -10,51 +10,62 @@
 #include "irc.h"
 #include "mldap.h"
 #include "http.h"
+#include "log.h"
 
-void rutinaDeError(char *string);
+void rutinaDeError(char *string, int log);
 SOCKET establecerConexionEscucha(in_addr_t nDireccionIP, in_port_t nPort);
 SOCKET establecerConexionServidor(in_addr_t nDireccionIP, in_port_t nPort, SOCKADDR_IN *their_addr);
 int atenderConsulta(SOCKET sockfd, ldapObj ldap, int cantidadConexiones);
 int conectarQueryManager (in_addr_t nDireccionIP, in_port_t nPort);
 
 configuracion config;
+mutex_t logMutex;
 
 int main()
 {
     SOCKET sockQP;
-    
+    int log;
     ldapObj ldap;
     
     fd_set fdMaestro;
     fd_set fdLectura;
     struct timeval timeout;
     int fdMax, cli, cantidadConexiones;
-
+    mode_t modeOpen = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
     fdMax = cli = cantidadConexiones = 0;
+
+    /*Se inicializa el mutex*/
+    mutex_init(&logMutex, USYNC_THREAD, NULL);
+
+    /*Se crea el archivo log*/
+    if ((log = open("log.txt", O_CREAT | O_TRUNC | O_WRONLY, modeOpen)) < 0)
+        rutinaDeError("Crear archivo Log", log);
+
+    WriteLog(log, "Query Processor", getpid(), thr_self(), "Inicio de ejecucion", "INFO");
     
-    /*Lectura de Archivo de Configuracion*/
-    printf("Se leera archivo de configuracion. ");
-    if (leerArchivoConfiguracion(&config) < 0)
-      rutinaDeError("Lectura Archivo de configuracion");
-    printf("Leido OK.\n");
+   /*Lectura de Archivo de Configuracion*/
+    WriteLog(log, "Query Processor", getpid(), thr_self(), "Se leera archivo de configuracion", "INFO");
+    if (leerArchivoConfiguracion(&config) != 0)
+       rutinaDeError("Lectura Archivo de configuracion", log);
+    WriteLog(log, "Query Processor", getpid(), thr_self(), "Leido OK", "INFOFIN");
 
     /*Se realiza la conexion a la base ldap*/
-    printf("Se realizara la conexion a ldap. ");
+    WriteLog(log, "Query Processor", getpid(), thr_self(), "Se realizara la conexion a ldap", "INFO");
     if (establecerConexionLDAP(&ldap, config) < 0)
-      rutinaDeError("No se pudo establecer la conexion LDAP.");	
-    printf("Conectado OK\n");
+      rutinaDeError("No se pudo establecer la conexion LDAP.", log);
+    WriteLog(log, "Query Processor", getpid(), thr_self(), "Conectado OK", "INFOFIN");
 
     /*Se realiza handshake con el QM, enviando el tipo de recurso*/
-    printf("Se realizara el Handshake con Query Manager. ");
-    if (conectarQueryManager(config.ipQM, config.puertoQM) < 0)
-        rutinaDeError("Conectar Query Manager");
-    printf("Realizado OK.\n");
+    WriteLog(log, "Query Processor", getpid(), thr_self(), "Se realizara el Handshake con Query Manager", "INFO");
+    if (conectarQueryManager(config.ipQM, config.puertoQM, log) < 0)
+        rutinaDeError("Conectar Query Manager", log);
+    WriteLog(log, "Query Processor", getpid(), thr_self(), "Realizado OK", "INFOFIN");
 
     /*Se establece la conexion de escucha*/
-    printf("Se establecera conexion de escucha. ");
+    WriteLog(log, "Query Processor", getpid(), thr_self(), "Se establecera la conexion de escucha", "INFO");
     if ((sockQP = establecerConexionEscucha(INADDR_ANY, config.puerto)) < 0)
-      rutinaDeError("Establecer conexion de escucha");
-    printf("Conexion establecida OK.\n");
+      rutinaDeError("Establecer conexion de escucha", log);
+    WriteLog(log, "Query Processor", getpid(), thr_self(), "Realizado OK", "INFOFIN");
 
     putchar('\n');
     FD_ZERO (&fdMaestro);
@@ -71,11 +82,12 @@ int main()
         FD_ZERO(&fdLectura);
         memcpy(&fdLectura, &fdMaestro, sizeof(fdMaestro));
 
-        printf("Esperando nuevas peticiones...\n");
+        WriteLog(log, "Query Processor", getpid(), thr_self(), "Esperando nuevas peticiones...", "INFOFIN");
+        putchar('\n');
         rc = select(fdMax+1, &fdLectura, NULL, NULL, &timeout);
 
         if (rc < 0)
-            rutinaDeError("select");
+            rutinaDeError("select", log);
         if (rc == 0)
         {
         /*SELECT TIMEOUT*/
@@ -91,6 +103,8 @@ int main()
                     if (cli == sockQP)
                     /*Nueva conexion detectada -> Thread en front end*/
                     {
+                        char text[60];
+
                         /*Cliente*/
                         SOCKET sockCliente;
                         SOCKADDR_IN dirCliente;
@@ -99,31 +113,33 @@ int main()
                         int mode = 0x00;
 
                         memset(descriptorID, '\0', DESCRIPTORID_LEN);
-                       
+
                         /*Acepta la conexion entrante. Thread en el front end*/
                         sockCliente = accept(sockQP, (SOCKADDR *) &dirCliente, &nAddrSize);
                         if (sockCliente == INVALID_SOCKET)
                         {
-                            perror("No se pudo conectar cliente");
+                            WriteLog(log, "Query Processor", getpid(), thr_self(), "No se pudo conectar cliente", "ERROR");
                             continue;
                         }
 
-                        printf("Conexion aceptada de %s.\n", inet_ntoa(dirCliente.sin_addr));
+                        sprintf(text, "Conexion aceptada de %s.\n", inet_ntoa(dirCliente.sin_addr));
+                        WriteLog(log, "Query Processor", getpid(), thr_self(), text, "INFOFIN");
 
                         /*Recibe el IRC con el permiso de peticion de atender request*/
-                        printf("Se recibira Handshake. ");
+                        WriteLog(log, "Query Processor", getpid(), thr_self(), "Se recibira handshake", "INFO");
                         if (ircRequest_recv (sockCliente, NULL, descriptorID, &mode) < 0)
                         {
-                            printf("Error.\n\n");
+                            WriteLog(log, "Query Processor", getpid(), thr_self(), "Error", "ERROR");
+                            putchar('\n');
                             close(sockCliente);
                             continue;
                         }
-                        printf("Recibido OK.\n");
+                        WriteLog(log, "Query Processor", getpid(), thr_self(), "Recibido OK", "INFOFIN");
 
                         /*Si se pueden realizar conexiones*/
                         if (mode == IRC_REQUEST_POSIBLE)
                         {
-                            printf("Se ha recibido un permiso de peticion.\n");
+                            WriteLog(log, "Query Processor", getpid(), thr_self(), "Se a recibido un permiso de peticion", "INFOFIN");
 
                             /*Si no hay conexiones disponibles*/
                             if (!(cantidadConexiones < config.cantidadConexiones))
@@ -132,19 +148,20 @@ int main()
                                 mode = IRC_RESPONSE_POSIBLE;                        
 
                             /*Responde el IRC con codigo de error*/
-                            printf("Se respondera permiso de peticion. ");
+                            WriteLog(log, "Query Processor", getpid(), thr_self(), "Se respondera permiso de peticion", "INFO");
                             if (ircResponse_send(sockCliente, descriptorID, NULL, 0, mode) < 0)
                             {
-                                printf("Error.\n\n");
+                                WriteLog(log, "Query Processor", getpid(), thr_self(), "Error", "ERROR");
                                 close(sockCliente);
                                 continue;
                             }
-                            printf("Respondido OK.\n");
+                            WriteLog(log, "Query Processor", getpid(), thr_self(), "Respondido OK", "INFOFIN");
 
                             /*Si no fue posible cierra conexion.*/
                             if (mode == IRC_RESPONSE_NOT_POSIBLE)
                             {
-                                printf("Conexion a sido rechazada.\n\n");
+                                WriteLog(log, "Query Processor", getpid(), thr_self(), "Conexion a sido rechazada", "INFOFIN");
+                                putchar('\n');
                                 close(sockCliente);
                             }
                             /*Si fue posible agrega para atender su request*/
@@ -155,24 +172,25 @@ int main()
                                 if (sockCliente > fdMax)
                                         fdMax = sockCliente;
                                 cantidadConexiones++;
-                                printf("Conexion a sido aceptada.\n\n");
+                                WriteLog(log, "Query Processor", getpid(), thr_self(), "Conexion a sido aceptada", "INFOFIN");
+                                putchar('\n');
                             }
                         }
                         else if(mode == IRC_REQUEST_IS_ALIVE)
                         {
-                            printf("Se a recibido una confirmacion de vida.\n");
+                            /*printf("Se a recibido una confirmacion de vida.\n");*/
 
                             mode = IRC_RESPONSE_IS_ALIVE;
 
                             /*Se respondera que se sigue conectado*/
-                            printf("Se respondera confirmacion de vida. ");
+                            /*printf("Se respondera confirmacion de vida. ");*/
                             if (ircResponse_send(sockCliente, descriptorID, NULL, 0, mode) < 0)
                             {
-                                printf("Error.\n\n");
+                                /*printf("Error.\n\n");*/
                                 close(sockCliente);
                                 continue;
                             }
-                            printf("Respondido OK.\n");
+                            /*printf("Respondido OK.\n");*/
 
                             close(sockCliente);
                         }
@@ -181,20 +199,24 @@ int main()
                     /*Cliente detectado -> esta enviando consultas*/
                     {
                         int control;
+                        char text[60];
 
-                        printf("Se atendera peticion del Cliente.\n");
+                        WriteLog(log, "Query Processor", getpid(), thr_self(), "Se atendera la peticion del cliente", "INFOFIN");
 
                         control = atenderConsulta(cli, ldap, cantidadConexiones);
 
                         if (control == 2)
-                            printf("Se a llegado al tope conexiones, se rechaza el pedido.");
+                            sprintf(text, "Se a llegado al tope conexiones, se rechaza el pedido.");
                         else if (control == 1)
-                            printf("Se a solicitado un recurso que no atiende este Query Processor.");
+                            sprintf(text, "Se a solicitado un recurso que no atiende este Query Processor.");
                         else if (control == 0)
-                                  printf("Atencion de cliente finalizada satisfactoriamente.");
+                            sprintf(text, "Atencion de cliente finalizada satisfactoriamente.");
                         else if (control < 0)
-                                printf("Hubo un error al atender Cliente.");
-                        printf(" Se cierra conexion.\n\n");
+                            sprintf(text, "Hubo un error al atender Cliente.");
+                        strcat(text, " Se cierra conexion");
+
+                        WriteLog(log, "Query Processor", getpid(), thr_self(), text, "INFOFIN");
+                        putchar('\n');
                    
                         /*Eliminar cliente y actualizar nuevo maximo*/
                         close(cli);
@@ -242,36 +264,35 @@ int atenderConsulta(SOCKET sockCliente, ldapObj ldap, int cantidadConexiones)
 	unsigned long len = 0;
 	PLDAP_RESULT_SET resultSet = NULL;
         unsigned int cantBloques = 0;
-        struct timeval timeout = {config.tiempoDemora, 0};
 
         memset(&getInfo, '\0', sizeof(getInfo));
         memset(descriptorID, '\0', sizeof(descriptorID));
 
 	/*Recibe las palabras a buscar*/
-        printf("Se recibiran las palabras a buscar. ");
+        WriteLog(log, "Query Processor", getpid(), thr_self(), "Se recibiran las palabras a buscar", "INFO");
 	if (ircRequest_recv (sockCliente, (void *) &getInfo, descriptorID, &mode) < 0)
 	{
-            printf("Error.\n");
+            WriteLog(log, "Query Processor", getpid(), thr_self(), "Error", "ERROR");
             return -1;
 	}
-        printf("Recibidas OK.\n");
+        WriteLog(log, "Query Processor", getpid(), thr_self(), "Recibidas OK", "INFOFIN");
 
         if (mode != IRC_REQUEST_UNICOQP)
         {
             /*Si no hay concordancia con los tipos de recursos que se enviaron y que se atienden*/
-            if ( (config.tipoRecurso == RECURSO_ARCHIVOS && mode != IRC_REQUEST_ARCHIVOS) ||
-                    (config.tipoRecurso == RECURSO_WEB && !(mode == IRC_REQUEST_HTML || mode == IRC_REQUEST_CACHE)) )
+            if ( (config.tipoRecurso == 1 && mode != IRC_REQUEST_ARCHIVOS) ||
+                    (config.tipoRecurso == 0 && (mode != IRC_REQUEST_HTML || mode != IRC_REQUEST_CACHE)) )
             {
-                printf("El recurso pedido no es atendido por este Query Processor.\n");
+                WriteLog(log, "Query Processor", getpid(), thr_self(), "El recurso pedido no es atendido por este Query Processor", "INFOFIN");
 
                 /*Envia el IRC con codigo de error*/
-                printf("Se enviara Internal Service Error. ");
+                WriteLog(log, "Query Processor", getpid(), thr_self(), "Se enviara Internal Service Error", "INFO");
                 if (ircResponse_send(sockCliente, descriptorID, NULL, 0, IRC_RESPONSE_ERROR) < 0)
                 {
-                    printf("Error.\n");
+                    WriteLog(log, "Query Processor", getpid(), thr_self(), "Error", "ERROR");
                     return -1;
                 }
-                printf("Enviado OK.\n");
+                WriteLog(log, "Query Processor", getpid(), thr_self(), "Recibidas OK", "INFOFIN");
                 return 1;
             }
         }
@@ -279,29 +300,27 @@ int atenderConsulta(SOCKET sockCliente, ldapObj ldap, int cantidadConexiones)
         {
             if (getInfo.searchType == WEB)
                 mode = IRC_REQUEST_HTML;
-            else if (getInfo.searchType == CACHE)
-                mode = IRC_REQUEST_CACHE;
             else
                 mode = IRC_REQUEST_ARCHIVOS;
         }
         
 	/*Realiza la busqueda*/
-        printf("Se realizara la busqueda en ldap. ");
+        WriteLog(log, "Query Processor", getpid(), thr_self(), "Se realizara la busqueda en ldap", "INFO");
 	if ((resultSet = consultarLDAP(&ldap, getInfo.queryString)) == NULL)
 	{
-            printf("Error al consultar ldap.\n");
+            WriteLog(log, "Query Processor", getpid(), thr_self(), "Error al consultar ldap", "ERROR");
             return -1;
 	}
 
 	/*Prepara la informacion a enviar por el IRC*/
 	if ((armarPayload(resultSet, &resultados, mode, &cantBloques)) < 0)
 	{
-            printf("Error al armar payload.\n");
+            WriteLog(log, "Query Processor", getpid(), thr_self(), "Error al armar el payload", "ERROR");
             ldapFreeResultSet(resultSet);
             return -1;
 	}
 
-        printf("Busqueda realizada OK.\n");
+        WriteLog(log, "Query Processor", getpid(), thr_self(), "Busqueda realizada", "INFOFIN");
 
         /*Indentifica el tipo de busqueda*/
 	if (mode == IRC_REQUEST_HTML)
@@ -319,18 +338,15 @@ int atenderConsulta(SOCKET sockCliente, ldapObj ldap, int cantidadConexiones)
             len = sizeof(hostsCodigo);
 	    mode = IRC_RESPONSE_CACHE;
 	}
-
-        /*Demora el tiempo especificado en el archivo de configuracion*/
-        select(0, 0, 0, 0, &timeout);
-
+        
 	/*Envia el IRC con los datos encontrados*/
-        printf("Se enviara respuesta. ");
+        WriteLog(log, "Query Processor", getpid(), thr_self(), "Se enviara respuesta", "INFO");
 	if (ircResponse_send(sockCliente, descriptorID, resultados, len, mode) < 0)
 	{
-            printf("Error.\n");
+            WriteLog(log, "Query Processor", getpid(), thr_self(), "Error", "ERROR");
             return -1;
 	}
-        printf("Respuesta enviada OK.\n");
+        WriteLog(log, "Query Processor", getpid(), thr_self(), "Respuesta enviada OK", "INFOFIN");
 
 	free(resultados);
 	
@@ -347,25 +363,25 @@ int conectarQueryManager (in_addr_t nDireccionIP, in_port_t nPort)
 
     memset(buffer, '\0', sizeof(buffer));
 
-    printf("Conectando Query Manager. ");
+    WriteLog(log, "Query Processor", getpid(), thr_self(), "Conectando Query Manager", "INFOFIN");
     sockQM = establecerConexionServidor(nDireccionIP, nPort, &their_addr);
     if (sockQM == INVALID_SOCKET)
     {
-        printf("Error.\n");
+        WriteLog(log, "Query Processor", getpid(), thr_self(), "Error", "ERROR");
         return -1;
     }
-    printf("OK.\n");
+    WriteLog(log, "Query Processor", getpid(), thr_self(), "Conexion OK", "INFOFIN");
 
     sprintf(buffer, "%s:%d-%d", inet_ntoa(*(IN_ADDR *) &config.ip), ntohs(config.puerto), config.tipoRecurso);
 
-    printf("Realizando Handshake. ");
+    WriteLog(log, "Query Processor", getpid(), thr_self(), "Realizando Handshake", "INFOFIN");
     if (ircRequest_send(sockQM, buffer, sizeof(buffer), descID, IRC_HANDSHAKE_QP) < 0)
     {
-        printf("Error.\n");
+        WriteLog(log, "Query Processor", getpid(), thr_self(), "Error", "ERROR");
         close(sockQM);
         return -1;
     }
-    printf("OK.\n");
+    WriteLog(log, "Query Processor", getpid(), thr_self(), "Handshake OK", "INFOFIN");
 
     close(sockQM);
 
@@ -377,7 +393,7 @@ SOCKET establecerConexionServidor(in_addr_t nDireccionIP, in_port_t nPort, SOCKA
     SOCKET sockfd;
 
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
-        rutinaDeError("socket");
+        return -1;
 
     their_addr->sin_family = AF_INET;
     their_addr->sin_port = nPort;
@@ -385,7 +401,7 @@ SOCKET establecerConexionServidor(in_addr_t nDireccionIP, in_port_t nPort, SOCKA
     memset(&(their_addr->sin_zero),'\0',8);
 
     /*if (connect(sockfd, (struct sockaddr *)their_addr, sizeof(struct sockaddr)) == -1)
-        rutinaDeError("connect");*/
+        return -1;*/
 
     while ( connect (sockfd, (SOCKADDR *)their_addr, sizeof(SOCKADDR)) == -1 && errno != EISCONN )
         if ( errno != EINTR )
@@ -418,11 +434,11 @@ SOCKET establecerConexionEscucha(in_addr_t nDireccionIP, in_port_t nPort)
 
         /*Impide el error "addres already in use" y setea non blocking el socket*/
         if (setsockopt(sockfd,SOL_SOCKET,SO_REUSEADDR,&yes,sizeof(int)) == -1)
-            rutinaDeError("Setsockopt");
+           return -1;
 
         /*if (ioctlsocket(sockfd, FIONBIO, &NonBlock) == SOCKET_ERROR)
         {
-        rutinaDeError("ioctlsocket");
+        return -1;
         }*/
 
         addrServidorWeb.sin_family = AF_INET;
@@ -435,21 +451,27 @@ SOCKET establecerConexionEscucha(in_addr_t nDireccionIP, in_port_t nPort)
         {
             /*Pone el puerto a la escucha de conexiones entrantes*/
             if (listen(sockfd, SOMAXCONN) == -1)
-                rutinaDeError("Listen");
+                return -1;
             else
             return sockfd;
         }
         else
-            rutinaDeError("Bind");
+            return -1;
     }
 
     return INVALID_SOCKET;
 }
 
-void rutinaDeError(char* error)
+void rutinaDeError(char* error, int log)
 {
     printf("\r\nError: %s\r\n", error);
     printf("Codigo de Error: %d\r\n\r\n", errno);
     perror(error);
+
+    /*Mutua exclusion*/
+    mutex_lock(&logMutex);
+    WriteLog(log, "Query Processor", getpid(), thr_self(), error, "ERROR");
+    mutex_unlock(&logMutex);
+
     exit(EXIT_FAILURE);
 }
